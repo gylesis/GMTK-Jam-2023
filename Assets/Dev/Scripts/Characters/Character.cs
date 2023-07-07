49bfd3034f7233b089d24ecc061b3235a38408ff
dan2315 <danilka2315.g@gmail.com>
2023-07-08 03:53:06 +0600

    character jumping

--- Assets/Dev/Scripts/Characters/Character.cs
+++ Assets/Dev/Scripts/Characters/Character.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UniRx;
 using UnityEngine;
 
 namespace Dev.Scripts.Characters
@@ -8,25 +9,25 @@ namespace Dev.Scripts.Characters
         [SerializeField] private float _speed;
         [SerializeField] private float _jumpStrength;
         [SerializeField] private float _gravityForceStrength;
+        [SerializeField] private LayerMask _ignoreMask;
+        [SerializeField] private Transform _checkGroundPoint;
         
         [SerializeField] private Rigidbody _rigidbody;
-        
+
+        [SerializeField] private bool _grounded;
+        [SerializeField] private bool _ableToCheck = true;
+
+        public LayerMask IgnoreMask => _ignoreMask;
+
         private void Awake()
         {
             Initialize();
         }
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Jump();
-            }
-        }
-
         private void FixedUpdate()
         {
             ApplyGravity();
+            CheckGround();
         }
 
         private void Initialize()
@@ -38,10 +39,27 @@ namespace Dev.Scripts.Characters
         {
             _rigidbody.AddForce(Vector3.down * _gravityForceStrength);
         }
+
         public void Jump()
         {
+            if(!_grounded) return;
+            
             _rigidbody.AddForce(Vector3.up * _jumpStrength);
+            _grounded = false;
+
+            _ableToCheck = false;
+            Observable.Timer(TimeSpan.FromMilliseconds(200)).TakeUntilDestroy(this).Subscribe(l =>
+            {
+                _ableToCheck = true;
+            });
         }
-        
+
+        private void CheckGround()
+        {
+            if (!_ableToCheck) return;
+            _grounded = Physics.Raycast(_checkGroundPoint.position, Vector3.down, out RaycastHit hit, 0.1f, _ignoreMask);
+            
+            if(hit.transform) Debug.Log(hit.transform.name);
+        }
     }
 }
---
+++ Assets/Dev/Scripts/Characters/CharacterAI.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Dev.Scripts.Characters
+{
+    public class CharacterAI : MonoBehaviour
+    {
+        [SerializeField] private Transform _cliffDetectPoint;
+        [SerializeField] private float _cliffDetectDistance;
+
+        [SerializeField] private float _spikeDistance;
+        
+        
+        [SerializeField] private Character _character;
+
+        private List<Spike> _spikes;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        private void FixedUpdate()
+        {
+            CheckForCliff();
+        }
+
+        private void Initialize()
+        {
+            //InitializeSpikes();
+        }
+        
+        private void CheckForCliff()
+        {
+            var cliffOccured = !Physics.Raycast(_cliffDetectPoint.position, Vector3.down, _cliffDetectDistance,_character.IgnoreMask);
+            if (cliffOccured)
+            {
+                _character.Jump();
+            }
+        }
+
+        private void InitializeSpikes()
+        {
+            _spikes = _spikes.OrderBy(spike => GetHorizontalDistanceTo(spike.transform)).ToList();
+        }
+
+        private void CheckForSpikes()
+        {
+            if (_spikes.First().transform )
+            {
+                
+            }  
+        }
+
+        private float GetHorizontalDistanceTo(Transform trans4m)
+        {
+            return _character.transform.position.x - trans4m.position.x;
+        }
+    }
+}
