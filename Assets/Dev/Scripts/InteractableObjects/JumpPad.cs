fa1f8c00c567a7ab96aa6ac9470b6e9d3c9ae3bd
dan2315 <danilka2315.g@gmail.com>
2023-07-09 02:46:32 +0600

    jmpad

--- Assets/Dev/Scripts/InteractableObjects/JumpPad.cs
+++ Assets/Dev/Scripts/InteractableObjects/JumpPad.cs
@@ -58,6 +58,7 @@ namespace Dev.Scripts.InteractableObjects
             if (other.TryGetComponent(out Character character))
             {
                 character.Jump(1.5f);
+                _pad.DOLocalMoveZ(1, 0.25f).SetEase(Ease.OutBounce).SetLoops(2, LoopType.Yoyo);
             }
         }
     }
